4b2760ab0ed5ccbb9585a0d199ad974a88bc7b66
supertepo <supertepo@outlook.com>
2024-05-31 12:08:29 -0600

    Update actualizacion de versiones

--- src/components/TicketForm.tsx
+++ src/components/TicketForm.tsx
@@ -1,37 +1,36 @@
-// src/components/TicketForm.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const TicketForm: React.FC = () => {
-    const [quantity, setQuantity] = useState<number>(0);
-    const pricePerTicket: number = 650; // Precio por boleto
+    const [quantity1, setQuantity1] = useState<number>(0);
+    const TicketAdulto: number = 450; // Precio por boleto
 
     const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuantity(parseInt(e.target.value, 10));
+        setQuantity1(parseInt(e.target.value, 10));
     };
 
-    const incrementQuantity = () => {
-        setQuantity(quantity + 1);
+    const incrementQuantity1 = () => {
+        setQuantity1(quantity1 + 1);
     };
 
-    const decrementQuantity = () => {
-        setQuantity(quantity - 1 < 0 ? 0 : quantity - 1);
+    const decrementQuantity1 = () => {
+        setQuantity1(quantity1 - 1 < 0 ? 0 : quantity1 - 1);
     };
 
-    const totalAmount: number = quantity * pricePerTicket;
+    const totalAmount: number = quantity1 * TicketAdulto;
 
     return (
         <div>
             <h2>Compra de Boletos</h2>
             <p>Cantidad de boletos</p>
             <div>
-                <button onClick={decrementQuantity} className="rounded dark:bg-teal-700 dark: text-gray-400 m-1 px-2.5 py-0.5">-</button>
+                <button onClick={decrementQuantity1} className="rounded dark:bg-teal-700 dark: text-gray-400 m-1 px-2.5 py-0.5">-</button>
                 <input
                     type="number"
                     min="0"
-                    value={quantity}
+                    value={quantity1}
                     onChange={handleQuantityChange}
                 />
-                <button onClick={incrementQuantity} className="rounded dark:bg-teal-700 dark: text-gray-400 m-1 px-2.5 py-0.5">+</button>
+                <button onClick={incrementQuantity1} className="rounded dark:bg-teal-700 dark: text-gray-400 m-1 px-2.5 py-0.5">+</button>
             </div>
             <p>Total a Pagar: ${totalAmount}</p>
         </div>
