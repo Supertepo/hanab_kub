859a8cd8d3a4dbb59c7a31b9ea0a98aee2956a11
supertepo <supertepo@outlook.com>
2024-05-31 13:26:56 -0600

    Update Funcion boleto menor y adulto nadamas

--- src/components/TicketForm.tsx
+++ src/components/TicketForm.tsx
@@ -1,40 +1,208 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const TicketForm: React.FC = () => {
-    const [quantity1, setQuantity1] = useState<number>(0);
-    const TicketAdulto: number = 450; // Precio por boleto
+  const [quantityAdulto, setQuantityAdulto] = useState<number>(0);
+  const TicketAdulto: number = 450;
 
-    const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuantity1(parseInt(e.target.value, 10));
-    };
+  const [quantityMenor, setQuantityMenor] = useState<number>(0);
+  const TicketMenor: number = 300;
 
-    const incrementQuantity1 = () => {
-        setQuantity1(quantity1 + 1);
-    };
+  const [quantityVIP, setQuantityVIP] = useState<number>(0);
+  const TicketVIP: number = 850;
 
-    const decrementQuantity1 = () => {
-        setQuantity1(quantity1 - 1 < 0 ? 0 : quantity1 - 1);
-    };
+  const [quantityVIPMenor, setQuantityVIPMenor] = useState<number>(0);
+  const TicketVIPMenor: number = 650;
 
-    const totalAmount: number = quantity1 * TicketAdulto;
+  const [quantityMembresia, setQuantityMembresia] = useState<number>(0);
+  const Membresia: number = 950;
 
-    return (
-        <div>
-            <h2>Compra de Boletos</h2>
-            <p>Cantidad de boletos</p>
-            <div>
-                <button onClick={decrementQuantity1} className="rounded dark:bg-teal-700 dark: text-gray-400 m-1 px-2.5 py-0.5">-</button>
-                <input
-                    type="number"
-                    min="0"
-                    value={quantity1}
-                    onChange={handleQuantityChange}
-                />
-                <button onClick={incrementQuantity1} className="rounded dark:bg-teal-700 dark: text-gray-400 m-1 px-2.5 py-0.5">+</button>
-            </div>
-            <p>Total a Pagar: ${totalAmount}</p>
+  const handleQuantityAdultoChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setQuantityAdulto(parseInt(e.target.value, 10));
+  };
+  const handleQuantityMenorChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setQuantityMenor(parseInt(e.target.value, 10));
+  };
+  const handleQuantityVIPChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuantityVIP(parseInt(e.target.value, 10));
+  };
+
+  const handleQuantityVIPMenorChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setQuantityVIPMenor(parseInt(e.target.value, 10));
+  };
+
+  const handleQuantityMembresiaChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setQuantityMembresia(parseInt(e.target.value, 10));
+  };
+
+  //Cantidad 1
+  const incrementQuantityAdulto = () => {
+    setQuantityAdulto(quantityAdulto + 1);
+  };
+
+  const decrementQuantityAdulto = () => {
+    setQuantityAdulto(quantityAdulto - 1 < 0 ? 0 : quantityAdulto - 1);
+  };
+  //Cantidad 2
+  const incrementQuantityMenor = () => {
+    setQuantityMenor(quantityMenor + 1);
+  };
+
+  const decrementQuantityMenor = () => {
+    setQuantityMenor(quantityMenor - 1 < 0 ? 0 : quantityMenor - 1);
+  };
+  //Cantidad 3
+  const incrementQuantityVIP = () => {
+    setQuantityVIP(quantityVIP + 1);
+  };
+
+  const decrementQuantityVIP = () => {
+    setQuantityVIP(quantityVIP - 1 < 0 ? 0 : quantityVIP - 1);
+  };
+  //Cantidad 4
+  const incrementQuantityVIPMenor = () => {
+    setQuantityVIPMenor(quantityVIPMenor + 1);
+  };
+
+  const decrementQuantityVIPMenor = () => {
+    setQuantityVIPMenor(quantityVIPMenor - 1 < 0 ? 0 : quantityVIPMenor - 1);
+  };
+  //Cantidad 5
+  const incrementQuantityMembresia = () => {
+    setQuantityMembresia(quantityMembresia + 1);
+  };
+
+  const decrementQuantityMembresia = () => {
+    setQuantityMembresia(quantityMembresia - 1 < 0 ? 0 : quantityMembresia - 1);
+  };
+
+  const totalAmount: number =
+    quantityAdulto * TicketAdulto +
+    quantityMenor * TicketMenor +
+    quantityVIP * TicketVIP +
+    quantityVIPMenor * TicketVIPMenor +
+    quantityMembresia * Membresia;
+  return (
+    <div className="max-w-xs mx-auto">
+      <h2>Compra de Boletos</h2>
+      <div>
+        <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-gary-900">
+          Cantidad de boletos Adulto
+        </p>
+        <div className="relative flex items-center max-w-[8rem]">
+          <button
+            onClick={decrementQuantityAdulto}
+            className="rounded-s-lg p-3 h-11 focus:ring-2 focus:outline-none dark:bg-teal-700 dark:border-gray-600"
+          >
+            <svg
+              className="w-3 h-3 text-gray-900 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 2"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h16"
+              />
+            </svg>
+          </button>
+          <input
+            type="text"
+            min="0"
+            value={quantityAdulto}
+            onChange={handleQuantityAdultoChange}
+            className=" border-x-0 h-11 text-center w-full py-2.5 text-gray-900 text-sm block dark:bg-teal-700 dark:text-white"
+          />
+          <button
+            onClick={incrementQuantityAdulto}
+            className="rounded-e-lg p-3 h-11 focus:ring-2 focus:outline-none dark:bg-teal-700 dark:border-gray-600"
+          >
+            <svg
+              className="w-3 h-3 text-gray-900 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 18"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 1v16M1 9h16"
+              />
+            </svg>
+          </button>
         </div>
-    );
+      </div>
+      <div>
+        <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-gary-900">
+          Cantidad de boletos Menor
+        </p>
+        <div className="relative flex items-center max-w-[8rem]">
+          <button
+            onClick={decrementQuantityMenor}
+            className="rounded-s-lg p-3 h-11 focus:ring-2 focus:outline-none dark:bg-teal-700 dark:border-gray-600"
+          >
+            <svg
+              className="w-3 h-3 text-gray-900 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 2"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h16"
+              />
+            </svg>
+          </button>
+          <input
+            type="text"
+            min="0"
+            value={quantityMenor}
+            onChange={handleQuantityMenorChange}
+            className=" border-x-0 h-11 text-center w-full py-2.5 text-gray-900 text-sm block dark:bg-teal-700 dark:text-white"
+          />
+          <button
+            onClick={incrementQuantityMenor}
+            className="rounded-e-lg p-3 h-11 focus:ring-2 focus:outline-none dark:bg-teal-700 dark:border-gray-600"
+          >
+            <svg
+              className="w-3 h-3 text-gray-900 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 18"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 1v16M1 9h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <p>Total a Pagar: ${totalAmount}</p>
+    </div>
+  );
 };
 
 export default TicketForm;
